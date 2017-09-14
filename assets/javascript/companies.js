db29fd479bd74d1c25ae3d684d8b097510d537c4
Nathaniel Spry <nspry1997@gmail.com>
2017-09-14 18:49:42 -0400

    added ajax calls to companies.js and added test button to index.html

--- assets/javascript/companies.js
+++ assets/javascript/companies.js
@@ -6,6 +6,22 @@ $(document).ready(function(){
 		url: queryURL,
 		method: "GET"
 	}).done(function(response) {
-		console.log(response);
+		for (var i = 0; i < response.body[0].games.length; i++) {
+			console.log(response.body[0].games[i]);
+		}
+	})
+
+	var requestObj = {
+    gameIds: [1185].join() 
+ 	}
+	$.ajax({
+		url: "https://rcb-igdb.herokuapp.com/games/" + JSON.stringify(requestObj.gameIds),
+		method: "GET",
+	}).done(function(response){
+		console.log(response)
+	})
+
+	$("#test-button").on("click", function() {
+		
 	})
 });
