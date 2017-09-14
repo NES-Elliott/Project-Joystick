c1542444c37e4f9066cf6ea1fde42b7e12807acb
Nathaniel Spry <nspry1997@gmail.com>
2017-09-14 16:09:09 -0400

    created companies.js script

---
+++ assets/javascript/companies.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+	var platform = "playstation";
+	var queryURL = "https://rcb-igdb.herokuapp.com/platform/" + platform;
+
+	$.ajax({
+		url: queryURL,
+		method: "GET"
+	}).done(function(response) {
+		console.log(response);
+	})
+});
