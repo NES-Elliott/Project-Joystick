206510316a91cab6eedfdd62c66603bf1b9196bc
Dan Testa <dantesta@Dans-MacBook-Pro-3.local>
2017-09-14 21:37:46 -0400

    DOM Template

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,1 +1,21 @@
-$(document).ready(function(){});
+    $(document).ready(function(){
+      $('.carousel').carousel();
+
+
+
+  // Initialize collapse button
+  $(".button-collapse").sideNav();
+  // Initialize collapsible (uncomment the line below if you use the dropdown variation)
+  //$('.collapsible').collapsible();
+
+
+	var platform = "linux";
+	var queryURL = "https://rcb-igdb.herokuapp.com/platform/" + platform;
+
+	$.ajax({
+		url: queryURL,
+		method: "GET"
+	}).done(function(response) {
+		console.log(response);
+	})
+});
